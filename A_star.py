dfde8d1bdfefcbb63f0b5f5012f39113d1773469
Parikshith-0101 <parikshithm2005@gmail.com>
2026-04-16 22:50:21 +0530

    Lab 4b

--- A_star.py
+++ A_star.py
@@ -156,3 +156,31 @@ if result:
 
     plt.title("A* Algorithm Visualization")
     plt.show()
+else:
+    print(f"No path found from {start_node}  to {stop_node}")
+
+#     Sample Input:
+#     graph = {
+#     'A': [('B', 11), ('C', 14), ('D', 7)],
+#     'B': [('A', 11), ('E', 15)],
+#     'C': [('A', 14), ('E', 8), ('D', 18), ('F', 10)],
+#     'D': [('A', 7), ('F', 25), ('C', 18)],
+#     'E': [('B', 15), ('C', 8), ('H', 9)],
+#     'F': [('G', 20), ('C', 10), ('D', 25)],
+#     'G': [],
+#     'H': [('E', 9), ('G', 10)]
+# }
+
+# heuristic = {
+#     'A': 40,
+#     'B': 32,
+#     'C': 25,
+#     'D': 35,
+#     'E': 19,
+#     'F': 17,
+#     'G': 0,
+#     'H': 10
+# }
+
+# start = 'A'
+# goal = 'G'
